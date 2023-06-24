653b53095288f6d083f794cdaf7043045e5bd36a
Faruk Abdo Feres <faruk@mottu.com.br>
2023-06-24 14:58:02 -0300

    Initial

---
+++ App1/App1/App1/Views/AboutPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btn_Save_Clicked(object sender, EventArgs e)
+        {
+            byte[] imageArray = null;
+            using (var imageStream = await sig_view1.GetImageStreamAsync(SignaturePad.Forms.SignatureImageFormat.Jpeg))
+            using (var memoryStream = new MemoryStream())
+            {
+                imageStream.CopyTo(memoryStream);
+                imageArray = memoryStream.ToArray();
+                //Send imageArray to API/Server
+            }
+
+            await DisplayAlert("Atenção", "Assinatura salva com sucesso", "OK");
+            //Clear signature
+            sig_view1.Clear();
+        }
+    }
+}
